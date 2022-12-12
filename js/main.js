1764351bc389300927ac8e38f34799af7f0a1db1
marufs <martinkuff@gmail.com>
2022-12-12 18:09:24 +0000

    Improved header at small screen width

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
 console.log("Finished loading");
+let menuVisible = false;
 
+
+function toggleMenu () {
+  let navBar = document.querySelector("nav");
+
+  if (menuVisible) {
+    menuVisible = false;
+    navBar.style.display = "none";
+    
+  } else {
+    menuVisible = true;
+    navBar.style.display = "flow-root";
+  }
+}
